bfa81ff8543b21602c54fb65cb6c6b5a865779c3
Simone <Simone@Simone-PC>
2016-02-18 17:58:30 -0200

    Alterações feitas baseadas no curso II do php da Alura - 18/02/2015

--- conecta.php
+++ conecta.php
@@ -1,5 +1,5 @@
 <?php
-//$conexao = mysqli_connect('localhost', 'simone', '442332si', 'loja');
+$conexao = mysqli_connect('localhost', 'simone', '442332si', 'loja');
 
-$conexao = mysqli_connect("mysql.hostinger.com.br", "u973571035_loja", "lojaloja", "u973571035_loja");
+//$conexao = mysqli_connect("mysql.hostinger.com.br", "u973571035_loja", "lojaloja", "u973571035_loja");
 
--- index.php
+++ index.php
@@ -1,3 +1,52 @@
-<?php include("cabecalho.php")?>
+<?php include("cabecalho.php"); ?>
+
+<?php
+if(isset($_GET["login"]) && $_GET["login"] == true){
+?>
+<p class="alert-success">Logado com Sucesso!</p>
+<?php 
+}
+?>
+
+<?php
+if(isset($_GET["login"]) && $_GET["login"] == false){
+?>
+<p class="alert-danger">Usuario ou Senha inválido!!</p>
+<?php
+}
+?>
+
 <h1>Bem vindo!</h1>
-<?php include("rodape.php")?>
+
+        <form class="form-horizontal" action="login.php" method="post" id="form_login">
+            <div class="row">
+
+                <div class="form-group-lg">
+                    <div>
+                        <label class="sr-only" id="email" for="email">Email</label>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12" id="div_input">
+                        <input type="email" name="email" class="form-control" placeholder="Email" required="autofocus">
+                    </div>
+                </div>
+
+                <div class="form-group-lg">
+                    <div>
+                        <label class="sr-only" for="senha">Senha</label>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12" id="div_input">
+                        <input type="password" name="senha" class="form-control" placeholder="Senha" required="autofocus">
+                    </div>
+                </div>
+
+                <div class="row form-group-lg" id="div_input">
+                    <div class="col-xs-offset-4 col-xs-4 col-sm-offset-4 col-sm-4 col-md-offset-4 col-md-4 text-left">
+                        <button class="btn btn-primary" type="submit">Logar</button>
+                    </div>
+                </div>  
+            </div>
+        </form>
+
+    </div>
+</section>
+<?php include("rodape.php") ?>
